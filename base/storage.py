77d386d4a126b49c4173a58563d607b3f4dd0086
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 16:24:02 +0330

    Create storage.py

---
+++ base/storage.py
@@ -0,0 +1,12 @@
+import os
+from datetime import datetime
+
+BASE_PATH = "uploads"
+
+
+def upload_to_by_date(instance, filename):
+    today = datetime.now()
+    timestamp = today.strftime("%Y%m%d%H%M%S")
+    file_extension = os.path.splitext(filename)[1]
+    new_filename = f"{timestamp}{file_extension}"
+    return os.path.join(f"{BASE_PATH}/{today.year}/", new_filename)
